742c589daa27dae7ecdd2b346f0b7cf03a34f017
shotasuzuki <shotasuzuki01@gmail.com>
2023-05-17 01:39:23 +0900

    ファイル追加

---
+++ teamDevelopment/team2305/src/main/java/school/Team2305Application.java
@@ -0,0 +1,13 @@
+package school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2305Application.class, args);
+	}
+
+}
