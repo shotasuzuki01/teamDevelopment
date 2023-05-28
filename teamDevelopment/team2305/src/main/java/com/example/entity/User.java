ad66ba8c604f28a02b20236d3ad2d698a63ceb56
coca0513 <aoym.tmktk@gmail.com>
2023-05-29 01:37:24 +0900

    Entity

--- teamDevelopment/team2305/src/main/java/com/example/entity/User.java
+++ teamDevelopment/team2305/src/main/java/com/example/entity/User.java
@@ -17,31 +17,74 @@ import lombok.Data;
 @Data
 @Table(name = "user")
 public class User implements Serializable {
-  /**
-   * ID
-   */
-  @Id
-  @Column(name = "id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  /**
-   * 名前
-   */
-  @Column(name = "name")
-  private String name;
-  /**
-   * フリガナ
-   */
-  @Column(name = "フリガナ")
-  private String furigana;
-  /**
-   * メールアドレス
-   */
-  @Column(name = "mail")
-  private String mail;
-  /**
-   * パスワード
-   */
-  @Column(name = "pass")
-  private String pass;
+
+	//ID
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userId")
+	private Integer userId;
+
+	//パスワード
+	@Column(name = "pass")
+	private String pass;
+
+	//ユーザー名
+	@Column(name = "userName", nullable = false)
+	private String userName;
+
+	//フリガナ
+	@Column(name = "フリガナ")
+	private String furigana;
+
+	//年齢
+	@Column(name = "userAge")
+	private Integer userAge;
+
+	//性別
+	@Column(name = "userSex")
+	private String userSex;
+
+	//誕生日
+	@Column(name = "birthPlace")
+	private String birthPlace;
+
+	//電話番号
+	@Column(name = "telephoneNumber")
+	private Integer telephoneNumber;
+
+	//メールアドレス
+	@Column(name = "mail")
+	private String mail;
+
+	//現役職
+	@Column(name = "activeDuty")
+	private String activeDuty;
+
+	//所属部署
+	@Column(name = "department")
+	private String department;
+
+	//入社日
+	@Column(name = "hireDate")
+	private Integer hireDate;
+
+	//前職
+	@Column(name = "beforejob")
+	private String beforejob;
+
+	//学歴
+	@Column(name = "educationalBg")
+	private String educationalBg;
+
+	//資格
+	@Column(name = "license")
+	private String license;
+
+	//趣味
+	@Column(name = "hobby")
+	private String hobby;
+
+	//自由欄
+	@Column(name = "free",length = 500)
+	private String free;
 }
