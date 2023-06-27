789a717dec9afefc32d5d6bb613f980578f36433
coca0513 <aoym.tmktk@gmail.com>
2023-06-28 02:07:52 +0900

    test

--- teamDevelopment/team2305/src/main/java/com/example/controller/myPageEdit.java
+++ teamDevelopment/team2305/src/main/java/com/example/controller/myPageEdit.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,22 +35,29 @@ public class myPageEdit {
 		UserAoyama user = userService.getUserById(id);
 	    model.addAttribute("user", user);
 		model.addAttribute("id", id);
+		int i = 0;
 		return "individualPage";
+
+
 	}
 
 	//個人ページ編集
 	@GetMapping("/individualPageEdit/{id}")
 	public String individualPageEdit(@PathVariable("id") Integer id, Model model) {
-		UserAoyama user = userService.getUserById(id);
-	    model.addAttribute("user", user);
-		model.addAttribute("id", id);
-		return "individualPageEdit";
+		UserAoyama user = userService.getUserById(id);	//取得
+	    model.addAttribute("user", user);				//読み込み
+		model.addAttribute("id", id);					//読み込み
+		return "individualPageEdit";					//上をどの画面に渡すか
 	}
 
 
 	@PostMapping("/individualPageUpdate/{id}")
-    public String updateIndividualPage(@PathVariable("id") Integer id, @ModelAttribute("user") UserAoyama user) {
-        // データの更新処理を実行
+    public String updateIndividualPage(@PathVariable("id") Integer id,@ModelAttribute("user") @Validated UserAoyama user,BindingResult bindingResult)
+	{
+		if (bindingResult.hasErrors()) {
+            return "individualPageEdit";
+        }
+		// データの更新処理を実行
         userService.updateUser(user);
 
         // 更新後のページにリダイレクト
